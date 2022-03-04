7ce7b59f01d98f506917d26d36963789f01c9e32
makewheels <33256842+makewheels@users.noreply.github.com>
2022-03-04 21:26:29 +0800

    init

---
+++ src/main/java/com/github/makewheels/video2022/Video2022Application.java
@@ -0,0 +1,13 @@
+package com.github.makewheels.video2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Video2022Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Video2022Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/makewheels/video2022/Video2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.makewheels.video2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Video2022ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
