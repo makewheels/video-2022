9b2bf455a193b60f63b7fc9c6f89dab246458d83
makewheels <makewheels@github.com>
2022-03-16 22:58:03 +0800

    video plus

---
+++ src/main/java/com/github/makewheels/video2022/video/VideoService.java
@@ -0,0 +1,8 @@
+package com.github.makewheels.video2022.video;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class VideoService {
+
+}
---
+++ src/main/java/com/github/makewheels/video2022/video/VideoStatus.java
@@ -0,0 +1,8 @@
+package com.github.makewheels.video2022.video;
+
+public class VideoStatus {
+    public static final String CREATE = "CREATE";
+    public static final String ORIGINAL_FILE_READY = "ORIGINAL_FILE_READY";
+    public static final String TRANSCODING = "TRANSCODING";
+    public static final String READY = "READY";
+}
