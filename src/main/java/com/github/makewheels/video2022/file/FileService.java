a45c568e6597ef5c1041d1800b8c8acaa7858943
makewheels <makewheels@github.com>
2022-03-19 11:30:14 +0800

    预签名上传put请求

--- src/main/java/com/github/makewheels/video2022/file/FileService.java
+++ src/main/java/com/github/makewheels/video2022/file/FileService.java
@@ -1,18 +1,31 @@
 package com.github.makewheels.video2022.file;
 
+import com.alibaba.fastjson.JSONObject;
 import com.github.makewheels.usermicroservice2022.User;
+import com.github.makewheels.video2022.response.Result;
+import com.qcloud.cos.COSClient;
+import com.qcloud.cos.ClientConfig;
+import com.qcloud.cos.auth.BasicCOSCredentials;
+import com.qcloud.cos.auth.COSCredentials;
+import com.qcloud.cos.http.HttpMethodName;
+import com.qcloud.cos.http.HttpProtocol;
+import com.qcloud.cos.region.Region;
 import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.Date;
-import java.util.TreeMap;
 
 @Service
 public class FileService {
     @Resource
     private MongoTemplate mongoTemplate;
+
+    private String bucket = "video-2022-1253319037";
+
+    private COSClient cosClient;
 
     public File create(User user, String originalFilename) {
         File file = new File();
@@ -24,56 +37,41 @@ public class FileService {
         return file;
     }
 
-    public void getUploadCredential(User user, String fileId) {
-        TreeMap<String, Object> config = new TreeMap<>();
+    private COSClient getCOSClient() {
+        if (cosClient != null)
+            return cosClient;
+        String secretId = "AKIDqVv61h7IEvMXVGm22mHXXHm10kFUTDhv";
+        String secretKey = "1mJbeRyHK7ewylIZbNt9AwTlNlunQq23";
+        COSCredentials credentials = new BasicCOSCredentials(secretId, secretKey);
 
-        try {
-            config.put("secretId", "SecretId");
-            config.put("secretKey", "SecretKey");
+        ClientConfig clientConfig = new ClientConfig();
+        clientConfig.setRegion(new Region("ap-beijing"));
+        clientConfig.setHttpProtocol(HttpProtocol.https);
+        clientConfig.setSocketTimeout(30 * 1000);
+        clientConfig.setConnectionTimeout(30 * 1000);
+        cosClient = new COSClient(credentials, clientConfig);
+        return cosClient;
+    }
 
-            // 临时密钥有效时长，单位是秒，默认 1800 秒，目前主账号最长 2 小时（即 7200 秒），
-            // 子账号最长 36 小时（即 129600）秒
-            config.put("durationSeconds", 1800);
+    private String getPreSignedUrl(String key, long time, HttpMethodName httpMethodName) {
+        Date expirationDate = new Date(System.currentTimeMillis() + time);
+        return getCOSClient()
+                .generatePresignedUrl(bucket, key, expirationDate, httpMethodName)
+                .toString();
+    }
 
-            // 换成您的 bucket
-            config.put("bucket", "examplebucket-1250000000");
-            // 换成 bucket 所在地区
-            config.put("region", "ap-guangzhou");
+    public Result<JSONObject> getUploadUrl(User user, String fileId) {
+        File file = mongoTemplate.findById(fileId, File.class);
+        if (file == null)
+            return null;
+        if (!StringUtils.equals(user.getId(), file.getUserId()))
+            return null;
 
-            // 这里改成允许的路径前缀，可以根据自己网站的用户登录态判断允许上传的具体路径
-            // 列举几种典型的前缀授权场景：
-            // 1、允许访问所有对象："*"
-            // 2、允许访问指定的对象："a/a1.txt", "b/b1.txt"
-            // 3、允许访问指定前缀的对象："a*", "a/*", "b/*"
-            // 如果填写了“*”，将允许用户访问所有资源；除非业务需要，否则请按照最小权限原则授予用户相应的访问权限范围。
-            config.put("allowPrefixes", new String[]{
-                    "exampleobject"
-            });
+        String url = getPreSignedUrl(file.getKey(), 30 * 60 * 1000, HttpMethodName.PUT);
 
-            // 密钥的权限列表。必须在这里指定本次临时密钥所需要的权限。
-            // 简单上传、表单上传和分块上传需要以下的权限，其他权限列表请看
-            // https://cloud.tencent.com/document/product/436/31923
-            String[] allowActions = new String[]{
-                    // 简单上传
-                    "name/cos:PutObject",
-                    // 表单上传、小程序上传
-                    "name/cos:PostObject",
-                    // 分块上传
-                    "name/cos:InitiateMultipartUpload",
-                    "name/cos:ListMultipartUploads",
-                    "name/cos:ListParts",
-                    "name/cos:UploadPart",
-                    "name/cos:CompleteMultipartUpload"
-            };
-            config.put("allowActions", allowActions);
-
-            Response response = CosStsClient.getCredential(config);
-            System.out.println(response.credentials.tmpSecretId);
-            System.out.println(response.credentials.tmpSecretKey);
-            System.out.println(response.credentials.sessionToken);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("uploadUrl", url);
+        return Result.ok(jsonObject);
     }
 
 }
