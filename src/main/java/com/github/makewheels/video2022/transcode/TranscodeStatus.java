4e7ef2ec869d9a245cd3f859f0b71f1385b59805
makewheels <makewheels@github.com>
2022-03-20 01:40:28 +0800

    三个视频接口迁移完成

--- src/main/java/com/github/makewheels/video2022/transcode/TranscodeStatus.java
+++ src/main/java/com/github/makewheels/video2022/transcode/TranscodeStatus.java
@@ -2,7 +2,8 @@ package com.github.makewheels.video2022.transcode;
 
 public class TranscodeStatus {
     public static final String CREATED = "CREATED";
-    public static final String WAITING = "WAITING";
-    public static final String PROCESSING = "PROCESSING";
-    public static final String FINISH = "FINISH";
+    public static final String PENDING = "PENDING";
+    public static final String RUNNING = "RUNNING";
+    public static final String SUCCESS = "SUCCESS";
+    public static final String FAILED = "FAILED";
 }
