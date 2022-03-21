f90c3af01f45830d3f2fc13c0ae8ae46515b8da3
makewheels <33256842+makewheels@users.noreply.github.com>
2022-03-21 11:55:06 +0800

    Tomcat started on port(s): 5022 (http) 5023 (http) with context path ''

---
+++ src/main/java/com/github/makewheels/video2022/etc/TomcatConfig.java
@@ -0,0 +1,22 @@
+package com.github.makewheels.video2022.etc;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatConfig {
+
+    private final int httpPort = 5023;
+
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> cookieProcessorCustomizer() {
+        return (TomcatServletWebServerFactory factory) -> {
+            Connector connector = new Connector();
+            connector.setPort(httpPort);
+            factory.addAdditionalTomcatConnectors(connector);
+        };
+    }
+}
