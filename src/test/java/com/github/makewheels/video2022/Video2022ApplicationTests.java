38d5f206da54efc089b2ecb7d4b159839d4c4a7e
makewheels <makewheels@github.com>
2022-04-05 14:58:23 +0800

    预加载cdn

--- src/test/java/com/github/makewheels/video2022/Video2022ApplicationTests.java
+++ src/test/java/com/github/makewheels/video2022/Video2022ApplicationTests.java
@@ -1,13 +1,19 @@
 package com.github.makewheels.video2022;
 
+import com.github.makewheels.video2022.transcode.TranscodeService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.annotation.Resource;
+
 @SpringBootTest
 class Video2022ApplicationTests {
+    @Resource
+    private TranscodeService transcodeService;
 
     @Test
     void contextLoads() {
+        transcodeService.onVideoReady("624be4bd8bd8926b18d541ed");
     }
 
 }
