29d0c8854535d780497a0628dacaf28437a72559
makewheels <33256842+makewheels@users.noreply.github.com>
2022-04-07 15:49:32 +0800

    http in https page

--- src/main/java/com/github/makewheels/video2022/intercepter/LoginInterceptor.java
+++ src/main/java/com/github/makewheels/video2022/intercepter/LoginInterceptor.java
@@ -9,6 +9,9 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Slf4j
 public class LoginInterceptor implements HandlerInterceptor {
@@ -17,12 +20,17 @@ public class LoginInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(
-            HttpServletRequest request, HttpServletResponse response, Object handler) {
+            HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws IOException, URISyntaxException {
         String token = request.getHeader("token");
         User user = userServiceClient.getUserByRequest(request);
         log.debug("token = {}, user = {}", token, JSON.toJSONString(user));
         if (user == null) {
             response.setStatus(403);
+
+            URI uri = new URI(request.getRequestURL().toString());
+            String target = uri.getScheme() + "://" + uri.getHost() + ":5021/user-micro-service-2022/login.html";
+            response.sendRedirect(target);
             return false;
         } else {
             response.setStatus(200);
