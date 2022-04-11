4b82d5e8ac98b5bdf57f21ab7547c6ded2fe50c0
makewheels <makewheels@github.com>
2022-04-11 21:12:59 +0800

    阿里云转码Service

--- src/main/java/com/github/makewheels/video2022/transcode/AliyunMpsService.java
+++ src/main/java/com/github/makewheels/video2022/transcode/AliyunMpsService.java
@@ -1,18 +1,24 @@
 package com.github.makewheels.video2022.transcode;
 
 import cn.hutool.core.codec.Base64;
+import cn.hutool.core.util.URLUtil;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.aliyun.mts20140618.Client;
-import com.aliyun.mts20140618.models.SubmitMediaInfoJobRequest;
-import com.aliyun.mts20140618.models.SubmitMediaInfoJobResponse;
-import com.aliyun.oss.common.comm.Protocol;
+import com.aliyun.mts20140618.models.*;
 import com.aliyun.teaopenapi.models.Config;
+import jdk.nashorn.internal.scripts.JO;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class AliyunMpsService {
+    @Value("${aliyun.oss.bucket}")
+    private String bucket;
+
     @Value("${aliyun.mps.accessKeyId}")
     private String accessKeyId;
     @Value("${aliyun.mps.secretKey}")
@@ -37,29 +43,91 @@ public class AliyunMpsService {
         return null;
     }
 
-    public static void main(String[] args) {
-        SubmitMediaInfoJobRequest mediaInfoRequest = new SubmitMediaInfoJobRequest();
-        mediaInfoRequest.setInput("{\"Bucket\":\"video-2022-dev\",\"Location\":" +
-                "\"oss-cn-beijing\",\"Object\":\"videos%2F6231de9a5bffa00422da71ce%" +
-                "2F6253dede8706fe314a47a54d%2Foriginal%2F6253dede8706fe314a47a54d.webm\"}");
-        AliyunMpsService aliyunMpsService = new AliyunMpsService();
-        Client client = aliyunMpsService.getClient();
-        if (client == null) return;
+    private String getInput(String key) {
+        JSONObject input = new JSONObject();
+        input.put("Bucket", bucket);
+        input.put("Location", "oss-cn-beijing");
+        input.put("Object", URLUtil.encode(key));
+        return input.toJSONString();
+    }
+
+    /**
+     * 获取媒体信息
+     *
+     * @param key
+     * @return
+     */
+    public SubmitMediaInfoJobResponse getMediaInfo(String key) {
+        SubmitMediaInfoJobRequest request = new SubmitMediaInfoJobRequest();
+        request.setInput(getInput(key));
+        SubmitMediaInfoJobResponse response = null;
         try {
-            SubmitMediaInfoJobResponse response = client.submitMediaInfoJob(mediaInfoRequest);
-
-            System.out.println(JSON.toJSONString(response.getBody()));
-
-            String body = JSON.toJSONString(response.getBody());
-            JSONObject jsonObject = JSON.parseObject(body);
-            JSONObject properties = jsonObject.getJSONObject("mediaInfoJob").getJSONObject("properties");
-            int width = Integer.parseInt(properties.getString("width"));
-            int height = Integer.parseInt(properties.getString("height"));
-            int duration = (int) (Double.parseDouble(properties.getString("duration")) * 1000);
-
+            response = getClient().submitMediaInfoJob(request);
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return response;
+//        String body = JSON.toJSONString(response.getBody());
+//        JSONObject jsonObject = JSON.parseObject(body);
+//        JSONObject properties = jsonObject.getJSONObject("mediaInfoJob").getJSONObject("properties");
+//        int width = Integer.parseInt(properties.getString("width"));
+//        int height = Integer.parseInt(properties.getString("height"));
+//        int duration = (int) (Double.parseDouble(properties.getString("duration")) * 1000);
+    }
+
+    /**
+     * 提交转码任务
+     *
+     * @param from
+     * @param to
+     * @param templateId
+     * @return
+     */
+    public SubmitJobsResponse submitTranscodeJob(String from, String to, String templateId) {
+        SubmitJobsRequest request = new SubmitJobsRequest()
+                .setInput(getInput(from))
+                .setOutputs("[{\"OutputObject\":\"" + URLUtil.encode(to) + "\",\"" +
+                        "TemplateId\":\"" + templateId + "\"}]")
+                .setOutputBucket(bucket)
+                .setPipelineId("6c126c07a9b34a85b7093e7bfa9c3ad9");
+        log.info("阿里云转码任务: from = " + from);
+        log.info("阿里云转码任务: to = " + to);
+        log.info("submitJobsRequest = " + JSON.toJSONString(request));
+        SubmitJobsResponse response = null;
+        try {
+            response = getClient().submitJobs(request);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        log.info("阿里云转码任务提交任务相应: SubmitJobsResponse = " + JSON.toJSONString(response));
+        return response;
+    }
+
+    /**
+     * 查询转码作业
+     *
+     * @param jobIds
+     * @return
+     */
+    public QueryJobListResponse queryJob(String jobIds) {
+        log.info("阿里云查询转码作业，jobIds = " + jobIds);
+        try {
+            return getClient().queryJobList(new QueryJobListRequest().setJobIds(jobIds));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 是否是已结束的状态
+     *
+     * @param jobState
+     * @return
+     */
+    private boolean isFinishedStatus(String jobState) {
+        return StringUtils.equalsAny(jobState, AliyunTranscodeStatus.TranscodeSuccess,
+                AliyunTranscodeStatus.TranscodeFail, AliyunTranscodeStatus.TranscodeCancelled);
     }
 
 }
