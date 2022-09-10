06da34f5805c0c8c39f2554cda68b063b9f8aef2
makewheels <33256842+makewheels@users.noreply.github.com>
2022-09-10 13:10:39 +0800

    现在必须要jdk11版本了，准备做1080和720切换

--- src/main/java/com/github/makewheels/video2022/transcode/aliyun/AliyunMpsService.java
+++ src/main/java/com/github/makewheels/video2022/transcode/aliyun/AliyunMpsService.java
@@ -8,11 +8,8 @@ import com.alibaba.fastjson.JSONObject;
 import com.aliyun.mts20140618.Client;
 import com.aliyun.mts20140618.models.*;
 import com.aliyun.teaopenapi.models.Config;
-import com.baidubce.services.media.model.CreateTranscodingJobResponse;
 import com.github.makewheels.video2022.transcode.Resolution;
-import jdk.nashorn.internal.scripts.JO;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
