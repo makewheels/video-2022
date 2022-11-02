0c4a0364493b6fe5ad2de7b278c0d802d5b45abc
makewheels <33256842+makewheels@users.noreply.github.com>
2022-11-02 22:55:38 +0800

    删除对象存储中的视频

--- src/main/java/com/github/makewheels/video2022/video/VideoDeleteTask.java
+++ src/main/java/com/github/makewheels/video2022/video/VideoDeleteTask.java
@@ -1,17 +1,22 @@
 package com.github.makewheels.video2022.video;
 
+import com.alibaba.fastjson.JSON;
+import com.aliyun.oss.model.OSSObjectSummary;
 import com.github.makewheels.video2022.file.FileService;
 import com.github.makewheels.video2022.utils.PathUtil;
 import com.github.makewheels.video2022.video.bean.Video;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 定时删除过期视频
  */
 @Component
+@Slf4j
 public class VideoDeleteTask {
     @Resource
     private VideoService videoService;
@@ -21,9 +26,15 @@ public class VideoDeleteTask {
     //    @Scheduled(cron = "")
     public void run() {
         List<Video> videos = videoService.getExpiredVideos(0, 5000);
+        log.info("删除任务，捞出这些要删除的视频: {}" + JSON.toJSONString(videoService));
         for (Video video : videos) {
             String transcodePath = PathUtil.getS3TranscodePrefix(video.getUserId(), video.getId());
-
+            //列举文件
+            List<OSSObjectSummary> objects = fileService.listObjects(transcodePath);
+            List<String> keys = objects.stream()
+                    .map(OSSObjectSummary::getKey).collect(Collectors.toList());
+            List<String> deletedKeys = fileService.deleteObjects(keys);
+            log.info("删除视频: deletedKeys = {}", JSON.toJSONString(deletedKeys));
         }
     }
 }
