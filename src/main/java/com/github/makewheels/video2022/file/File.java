e961f1eecdb8f0c6deb4c744de99f88333c5fb54
makewheels <33256842+makewheels@users.noreply.github.com>
2022-11-03 22:33:56 +0800

    改善File对象

--- src/main/java/com/github/makewheels/video2022/file/File.java
+++ src/main/java/com/github/makewheels/video2022/file/File.java
@@ -14,25 +14,31 @@ public class File {
     private String id;
     @Indexed
     private String userId;
+    @Indexed
+    private String videoId;
 
+    //原始文件名，只有用户上传的视频源文件才有
+    private String originalFilename;
+    // 63627b7e66445c2fe81c648a.mp4
+    private String filename;
+
+    // videos/62511690c3afe0646f9c670b/63627b7e66445c2fe81c648a/original/63627b7e66445c2fe81c648a.mp4
     private String key;
     private String extension;
 
-    private String originalFilename;
-
     private Long size;
+    @Indexed
     private String etag;
 
     @Indexed
     private String type;
 
     @Indexed
-    private String videoId;
-
-    @Indexed
     private String provider;
     @Indexed
     private String videoType;
+    @Indexed
+    private String storageClass;
 
     @Indexed
     private String status;
