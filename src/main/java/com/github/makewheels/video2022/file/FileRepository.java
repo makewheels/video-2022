91fb21c00ff06717d4c49d8e8223b62aa1b86333
makewheels <33256842+makewheels@users.noreply.github.com>
2022-11-05 02:52:43 +0800

    获取文件

---
+++ src/main/java/com/github/makewheels/video2022/file/FileRepository.java
@@ -0,0 +1,24 @@
+package com.github.makewheels.video2022.file;
+
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Repository;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Repository
+public class FileRepository {
+    @Resource
+    private MongoTemplate mongoTemplate;
+
+    public File findById(String id) {
+        return mongoTemplate.findById(id, File.class);
+    }
+
+    public List<File> findByTranscodeId(String transcodeId) {
+        return mongoTemplate.find(Query.query(Criteria.where("transcodeId").is(transcodeId)), File.class);
+
+    }
+}
