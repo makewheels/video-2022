d4a9fe6e5aa07a3afb684f568071e12b966d7bad
makewheels <33256842+makewheels@users.noreply.github.com>
2022-11-24 22:07:34 +0800

    DTO改造方案

--- src/main/java/com/github/makewheels/video2022/utils/RequestUtil.java
+++ src/main/java/com/github/makewheels/video2022/utils/RequestUtil.java
@@ -1,5 +1,7 @@
 package com.github.makewheels.video2022.utils;
 
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.TypeReference;
 import org.apache.commons.beanutils.BeanUtils;
 
 import javax.servlet.http.HttpServletRequest;
@@ -8,11 +10,21 @@ import java.util.Map;
 import java.util.Set;
 
 public class RequestUtil {
+    private static <T> T mapToBean(Map<String, String> map, Class<T> clazz) {
+        T t = null;
+        try {
+            t = clazz.getConstructor().newInstance();
+            BeanUtils.populate(t, map);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return t;
+    }
 
     /**
      * 根据url的参数，转为DTO对象
      */
-    public static <T> T requestToBean(HttpServletRequest request, Class<T> clazz) {
+    public static <T> T toDTO(HttpServletRequest request, Class<T> clazz) {
         Map<String, String[]> parameterMap = request.getParameterMap();
         Map<String, String> simpleHashMap = new HashMap<>(parameterMap.size());
         Set<String> keySet = parameterMap.keySet();
@@ -20,14 +32,19 @@ public class RequestUtil {
             String value = parameterMap.get(key)[0];
             simpleHashMap.put(key, value);
         }
-        T t = null;
-        try {
-            t = clazz.getConstructor().newInstance();
-            BeanUtils.populate(t, simpleHashMap);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return t;
+        return mapToBean(simpleHashMap, clazz);
     }
+
+    public static <T> T toDTO(String json, Class<T> clazz) {
+        TypeReference<Map<String, String>> typeReference = new TypeReference<Map<String, String>>() {
+        };
+        Map<String, String> map = JSONObject.parseObject(json, typeReference);
+        return mapToBean(map, clazz);
+    }
+
+    public static <T> T toDTO(JSONObject body, Class<T> clazz) {
+        return toDTO(body.toJSONString(), clazz);
+    }
+
 
 }
