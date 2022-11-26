01d910e406ef2e5e28da0b8ecc1e6f75406f0330
makewheels <33256842+makewheels@users.noreply.github.com>
2022-11-27 02:41:16 +0800

    RSA秘钥改造计划

---
+++ src/test/java/com/github/makewheels/video2022/GeneratePassword.java
@@ -0,0 +1,42 @@
+package com.github.makewheels.video2022;
+
+import cn.hutool.core.io.FileUtil;
+import com.github.makewheels.video2022.utils.RSAUtil;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+public class GeneratePassword {
+    @Test
+    public void generateKeyPairs() {
+        Map<String, String> map = RSAUtil.generateKeyPairs();
+        String privateKey = map.get("privateKey");
+        String publicKey = map.get("publicKey");
+        System.out.println("privateKey = " + privateKey);
+        System.out.println("publicKey = " + publicKey);
+    }
+
+    @Test
+    public void encrypt() {
+        String plain = "cJJx3LZgpiZkIrIC";
+        System.out.println("plainText = " + plain);
+
+        String publicKey = FileUtil.readUtf8String("C:\\Users\\thedoflin\\Downloads\\publicKey.txt");
+
+        String cipher = RSAUtil.encrypt(publicKey, plain);
+        System.out.println("cipher = " + cipher);
+
+    }
+
+    @Test
+    public void decrypt() {
+        String cipher = "jXtCCfW53qlgax3P5CVwpwA/ENi/ZNL4+BJfgwRAfxt/xyc6z52E09JRYil6+GB0SozQmU68wFlwlVx6OGipbK0eq6BVZe9KsJoSH58VBsGRdyiH2jW2+HGDZXS+er3WcYTOuqQKybuTABxh2DBf9O/DTvbNuGNQdxMVdewleXI=";
+        System.out.println("cipher = " + cipher);
+
+        String privateKey = FileUtil.readUtf8String("C:\\Users\\thedoflin\\Downloads\\privateKey.txt");
+
+        String plain = RSAUtil.decrypt(privateKey, cipher);
+        System.out.println("plain = " + plain);
+
+    }
+}
--- src/test/java/com/github/makewheels/video2022/Video2022ApplicationTests.java
+++ src/test/java/com/github/makewheels/video2022/Video2022ApplicationTests.java
@@ -1,18 +1,14 @@
 package com.github.makewheels.video2022;
 
-import com.github.makewheels.video2022.transcode.TranscodeCallbackService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import javax.annotation.Resource;
-
 @SpringBootTest
 class Video2022ApplicationTests {
-    @Resource
-    private TranscodeCallbackService transcodeCallbackService;
 
     @Test
     void contextLoads() {
+
     }
 
 }
