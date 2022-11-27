10a0c2e8d6a1221bd7b2f65ff5d7528a04c15ab0
makewheels <33256842+makewheels@users.noreply.github.com>
2022-11-27 21:13:09 +0800

    密码改造计划竣工

--- src/main/java/com/github/makewheels/video2022/etc/password/PasswordDecipher.java
+++ src/main/java/com/github/makewheels/video2022/etc/password/PasswordDecipher.java
@@ -4,12 +4,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
-import org.springframework.boot.env.OriginTrackedMapPropertySource;
 import org.springframework.core.Ordered;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.MapPropertySource;
 import org.springframework.core.env.MutablePropertySources;
-import org.springframework.core.env.PropertySource;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -18,6 +16,7 @@ import java.util.Map;
  * 密码解密器
  */
 @Slf4j
+
 public class PasswordDecipher implements BeanFactoryPostProcessor, Ordered {
 
     private final ConfigurableEnvironment environment;
@@ -28,22 +27,16 @@ public class PasswordDecipher implements BeanFactoryPostProcessor, Ordered {
 
     @Override
     public void postProcessBeanFactory(@NotNull ConfigurableListableBeanFactory beanFactory) {
-        String springProfileActive = environment.getProperty("spring.profile.active");
-
         MutablePropertySources propertySources = environment.getPropertySources();
-        for (PropertySource<?> propertySource : propertySources) {
-            if (propertySource instanceof OriginTrackedMapPropertySource) {
-                OriginTrackedMapPropertySource originTrackedMapPropertySource
-                        = (OriginTrackedMapPropertySource) propertySource;
-                Map<String, Object> source = originTrackedMapPropertySource.getSource();
-                source.forEach((key, value) -> {
-                    String property = environment.getProperty(key);
-                    Map<String, Object> map = new HashMap<>();
-                    map.put(key, "plain");
-                    propertySources.addFirst(new MapPropertySource(key, map));
-                });
-            }
-        }
+
+        String springProfileActive = environment.getProperty("spring.profile.active");
+        Map<String, String> plainTextMap = PasswordUtil.getPlainTextMap(springProfileActive);
+
+        plainTextMap.forEach((key, value) -> {
+            Map<String, Object> map = new HashMap<>();
+            map.put(key, value);
+            propertySources.addFirst(new MapPropertySource(key, map));
+        });
     }
 
     @Override
--- src/main/java/com/github/makewheels/video2022/etc/password/PasswordUtil.java
+++ src/main/java/com/github/makewheels/video2022/etc/password/PasswordUtil.java
@@ -2,11 +2,16 @@ package com.github.makewheels.video2022.etc.password;
 
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.setting.dialect.Props;
+import com.github.makewheels.video2022.utils.RSAUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.ResourceUtils;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 @Slf4j
 public class PasswordUtil {
@@ -14,49 +19,53 @@ public class PasswordUtil {
     /**
      * 获取解密私钥
      */
-    private String getPrivateKey(File configFile) {
-        //读取配置文件中的，私钥文件保存位置
-        Props props = new Props(configFile, StandardCharsets.UTF_8);
-        String filePath = props.getStr("password.privateKey.path");
-        File file = new File(filePath);
+    private static String getPrivateKey(String env) {
+        String path = null;
+        if (env.equals("dev")) {
+            path = "D:/workSpace/~keys/video-2022/privateKey.txt";
+        } else if (env.equals("prod")) {
+            path = "/root/keys/video-2022/privateKey.txt";
+        }
+        return FileUtil.readUtf8String(path);
+    }
 
-        //因为有dev环境和prod环境，私钥可能不存在，那就直接跳过
-        if (!file.exists()) return null;
-
-        //读取私钥文件，返回
-        return FileUtil.readUtf8String(file);
+    /**
+     * 获取密码文件
+     */
+    private static File getPasswordFile(String env) {
+        String path = null;
+        if (env.equals("dev")) {
+            path = "passwords-dev.properties";
+        } else if (env.equals("prod")) {
+            path = "passwords-prod.properties";
+        }
+        try {
+            return ResourceUtils.getFile("classpath:" + path);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     /**
      * 获得解密之后的map
      */
-    public Map<String, String> getPlainTextMap(String env) {
+    public static Map<String, String> getPlainTextMap(String env) {
         //加载解密私钥
-        String privateKey = getPrivateKey(new File(""));
-
-        //密码文件
-        Props passwords = new Props(new File(""), StandardCharsets.UTF_8);
+        String privateKey = getPrivateKey(env);
+        //加载密码文件
+        Props props = new Props(getPasswordFile(env), StandardCharsets.UTF_8);
 
         //遍历密码文件
-
-//        for (int i = 0; i < configLines.size(); i++) {
-//            String line = configLines.get(i);
-//            //跳过空行
-//            if (StringUtils.isEmpty(line)) continue;
-//
-//            // aliyun.oss.accessKeyId
-//            String key = line.split("=")[0];
-//            //拿着对应的key到秘钥文件里找到密文
-//            //LTAI5tbkS0Ti3ayiUkfAoPZL
-//            String cipher = passwords.getStr(key);
-//
-//            //有的配置有密码覆盖，有的没有，如果没有就跳过
-//            if (cipher == null) continue;
-//            //解密
-//            String plain = RSAUtil.decrypt(privateKey, cipher);
-//        }
-        return null;
-
+        Map<String, String> map = new HashMap<>(props.size());
+        Set<Object> keySet = props.keySet();
+        for (Object key : keySet) {
+            String cipher = props.getStr((String) key);
+            //解密，放入map
+            String plain = RSAUtil.decrypt(privateKey, cipher);
+            map.put((String) key, plain);
+        }
+        return map;
     }
 
 }
