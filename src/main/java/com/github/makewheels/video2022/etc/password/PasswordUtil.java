7d85ee139cc20f94272c4339f54fedd7403f8194
makewheels <33256842+makewheels@users.noreply.github.com>
2022-11-27 21:32:22 +0800

    密码改造计划竣工

--- src/main/java/com/github/makewheels/video2022/etc/password/PasswordUtil.java
+++ src/main/java/com/github/makewheels/video2022/etc/password/PasswordUtil.java
@@ -1,16 +1,15 @@
 package com.github.makewheels.video2022.etc.password;
 
 import cn.hutool.core.io.FileUtil;
-import cn.hutool.setting.dialect.Props;
+import com.github.makewheels.video2022.Video2022Application;
 import com.github.makewheels.video2022.utils.RSAUtil;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.util.ResourceUtils;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.nio.charset.StandardCharsets;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 import java.util.Set;
 
 @Slf4j
@@ -32,20 +31,16 @@ public class PasswordUtil {
     /**
      * 获取密码文件
      */
-    private static File getPasswordFile(String env) {
+    private static InputStream getPasswordFileInputStream(String env) {
         String path = null;
         if (env.equals("dev")) {
             path = "passwords-dev.properties";
         } else if (env.equals("prod")) {
             path = "passwords-prod.properties";
         }
-        try {
-            return ResourceUtils.getFile("classpath:" + path);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        return null;
+        return Video2022Application.class.getResourceAsStream("/" + path);
     }
+
 
     /**
      * 获得解密之后的map
@@ -54,16 +49,22 @@ public class PasswordUtil {
         //加载解密私钥
         String privateKey = getPrivateKey(env);
         //加载密码文件
-        Props props = new Props(getPasswordFile(env), StandardCharsets.UTF_8);
+        Properties properties = new Properties();
+        try {
+            properties.load(getPasswordFileInputStream(env));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         //遍历密码文件
-        Map<String, String> map = new HashMap<>(props.size());
-        Set<Object> keySet = props.keySet();
-        for (Object key : keySet) {
-            String cipher = props.getStr((String) key);
-            //解密，放入map
+        Map<String, String> map = new HashMap<>(properties.size());
+        Set<String> keySet = properties.stringPropertyNames();
+        for (String key : keySet) {
+            String cipher = properties.getProperty(key);
+            //解密
             String plain = RSAUtil.decrypt(privateKey, cipher);
-            map.put((String) key, plain);
+            //放入map
+            map.put(key, plain);
         }
         return map;
     }
