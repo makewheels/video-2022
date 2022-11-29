2902e03bfd24e2e4be7db9c195b249c3ba662549
makewheels <33256842+makewheels@users.noreply.github.com>
2022-11-30 01:05:23 +0800

    接入user微服务

--- src/main/java/com/github/makewheels/video2022/interceptor/WebConfiguration.java
+++ src/main/java/com/github/makewheels/video2022/interceptor/WebConfiguration.java
@@ -17,25 +17,28 @@ public class WebConfiguration implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(getLoginInterceptor())
                 .addPathPatterns("/**")
-                .excludePathPatterns("/upload.html")
-                .excludePathPatterns("/login.html")
-                .excludePathPatterns("/save-token.html")
-                .excludePathPatterns("/transfer-youtube.html")
-
                 .excludePathPatterns("/app/checkUpdate")
                 .excludePathPatterns("/favicon.ico")
                 .excludePathPatterns("/healthCheck")
 
+                .excludePathPatterns("/login.html")
+                .excludePathPatterns("/user/requestVerificationCode")
+                .excludePathPatterns("/user/submitVerificationCode")
+
+                .excludePathPatterns("/upload.html")
+                .excludePathPatterns("/save-token.html")
+                .excludePathPatterns("/transfer-youtube.html")
+
                 .excludePathPatterns("/transcode/baiduTranscodeCallback")
                 .excludePathPatterns("/transcode/aliyunCloudFunctionTranscodeCallback")
 
+                .excludePathPatterns("/watch")
                 .excludePathPatterns("/video/getWatchInfo")
                 .excludePathPatterns("/video/getVideoDetail")
                 .excludePathPatterns("/video/getVideoListByUserId")
                 .excludePathPatterns("/video/addWatchLog")
                 .excludePathPatterns("/video/getM3u8Content.m3u8")
                 .excludePathPatterns("/file/access")
-                .excludePathPatterns("/watch")
                 .excludePathPatterns("/statistics/getTrafficConsume")
         ;
     }
