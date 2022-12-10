69280fbbee779f15085668251f611d3813fcf198
makewheels <33256842+makewheels@users.noreply.github.com>
2022-12-10 23:03:04 +0800

    RequestUtil to DTO改造

---
+++ src/main/java/com/github/makewheels/video2022/context/Context.java
@@ -0,0 +1,10 @@
+package com.github.makewheels.video2022.context;
+
+import lombok.Data;
+
+@Data
+public class Context {
+    private String videoId;
+    private String clientId;
+    private String sessionId;
+}
