fd71818562bb62298a19a2a147062d246f393c79
makewheels <33256842+makewheels@users.noreply.github.com>
2022-12-11 23:42:11 +0800

    引入Progress和Heartbeat

---
+++ src/main/java/com/github/makewheels/video2022/watch/heartbeat/Heartbeat.java
@@ -0,0 +1,29 @@
+package com.github.makewheels.video2022.watch.heartbeat;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+public class Heartbeat {
+    @Id
+    private String id;
+
+    private String videoId;
+    private String clientId;
+    private String sessionId;
+    private String viewerId;
+
+    private String playerProvider;
+
+    private Date createTime;
+
+    private String type;
+    private String event;
+    private Long playerTime;
+    private String status;
+    private BigDecimal volume;
+
+}
---
+++ src/main/java/com/github/makewheels/video2022/watch/heartbeat/HeartbeatController.java
@@ -0,0 +1,10 @@
+package com.github.makewheels.video2022.watch.heartbeat;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("heartbeat")
+public class HeartbeatController {
+
+}
