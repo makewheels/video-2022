e0f68e94185864cf886a905db056ed7657882763
makewheels <33256842+makewheels@users.noreply.github.com>
2023-01-06 22:34:32 +0800

    引入api日志

---
+++ src/main/java/com/github/makewheels/video2022/api/Api.java
@@ -0,0 +1,22 @@
+package com.github.makewheels.video2022.api;
+
+import com.alibaba.fastjson.JSONObject;
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * 调用第三方api日志，主要记录返回结果，用于之后问题排查
+ */
+@Data
+public class Api {
+    private String type;            //类型，比如：钉钉，阿里云 mps api
+    private String code;            //第三方返回的状态码
+    private String message;         //第三方返回的错误码
+    private Boolean isSuccess;      //是否调用成功
+    private JSONObject request;
+    private JSONObject response;    //原始返回的json
+
+    private Date createTime;
+
+}
---
+++ src/main/java/com/github/makewheels/video2022/api/ApiType.java
@@ -0,0 +1,7 @@
+package com.github.makewheels.video2022.api;
+
+public interface ApiType {
+    String DING = "DING";
+    String ALIYUN_MPS = "ALIYUN_MPS";
+    String ALIYUN_CF_CPU = "ALIYUN_CF_CPU";
+}
