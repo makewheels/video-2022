447bba205d34f731b24fbefb86022a189e10db10
makewheels <33256842+makewheels@users.noreply.github.com>
2023-01-06 23:20:35 +0800

    启动日志格式调整

--- src/main/java/com/github/makewheels/video2022/startup/StartupService.java
+++ src/main/java/com/github/makewheels/video2022/startup/StartupService.java
@@ -2,7 +2,6 @@ package com.github.makewheels.video2022.startup;
 
 import cn.hutool.system.SystemUtil;
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -22,14 +21,11 @@ public class StartupService implements ApplicationListener<ContextRefreshedEvent
             StartupLog startupLog = new StartupLog();
             startupLog.setCreateTime(new Date());
 
-            JSONObject systemInfo = new JSONObject();
-            systemInfo.put("jvmInfo", JSON.parseObject(JSON.toJSONString(SystemUtil.getJvmInfo())));
-            systemInfo.put("hostInfo", JSON.parseObject(JSON.toJSONString(SystemUtil.getHostInfo())));
-            systemInfo.put("osInfo", JSON.parseObject(JSON.toJSONString(SystemUtil.getOsInfo())));
-            systemInfo.put("runtimeInfo", JSON.parseObject(JSON.toJSONString(SystemUtil.getRuntimeInfo())));
-            systemInfo.put("userInfo", JSON.parseObject(JSON.toJSONString(SystemUtil.getUserInfo())));
-
-            startupLog.setSystemInfo(systemInfo);
+            startupLog.setHostInfo(JSON.parseObject(JSON.toJSONString(SystemUtil.getHostInfo())));
+            startupLog.setJvmInfo(JSON.parseObject(JSON.toJSONString(SystemUtil.getJvmInfo())));
+            startupLog.setOsInfo(JSON.parseObject(JSON.toJSONString(SystemUtil.getOsInfo())));
+            startupLog.setRuntimeInfo(JSON.parseObject(JSON.toJSONString(SystemUtil.getRuntimeInfo())));
+            startupLog.setUserInfo(JSON.parseObject(JSON.toJSONString(SystemUtil.getUserInfo())));
 
             mongoTemplate.save(startupLog);
         }
