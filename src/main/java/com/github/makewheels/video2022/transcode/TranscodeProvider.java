a46abee3d844156f8ed8deec664bcd514c3f83f2
makewheels <33256842+makewheels@users.noreply.github.com>
2023-01-08 02:19:02 +0800

    发起480p转码

--- src/main/java/com/github/makewheels/video2022/transcode/TranscodeProvider.java
+++ src/main/java/com/github/makewheels/video2022/transcode/TranscodeProvider.java
@@ -1,7 +1,7 @@
 package com.github.makewheels.video2022.transcode;
 
-public class TranscodeProvider {
-    public static final String ALIYUN_MPS = "ALIYUN_MPS_TRANSCODE";
-    public static final String ALIYUN_CLOUD_FUNCTION = "ALIYUN_CLOUD_FUNCTION_TRANSCODE";
+public interface TranscodeProvider {
+    String ALIYUN_MPS = "ALIYUN_MPS_TRANSCODE";
+    String ALIYUN_CLOUD_FUNCTION = "ALIYUN_CLOUD_FUNCTION_TRANSCODE";
 
 }
