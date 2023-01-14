25791621572a1ef0d4c9fc187addaf2a12e4c064
makewheels <33256842+makewheels@users.noreply.github.com>
2023-01-15 03:51:51 +0800

    id生成

--- src/test/java/com/github/makewheels/video2022/TestId.java
+++ src/test/java/com/github/makewheels/video2022/TestId.java
@@ -13,7 +13,7 @@ public class TestId {
 
     @Test
     public void test() {
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 3; i++) {
             System.out.println(idService.nextId());
         }
     }
