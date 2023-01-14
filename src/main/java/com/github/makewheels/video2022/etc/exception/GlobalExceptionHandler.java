e7b15a32fa681ed8440690f8b217341c762d3913
makewheels <33256842+makewheels@users.noreply.github.com>
2023-01-15 00:50:50 +0800

    保存错误到数据库

--- src/main/java/com/github/makewheels/video2022/etc/exception/GlobalExceptionHandler.java
+++ src/main/java/com/github/makewheels/video2022/etc/exception/GlobalExceptionHandler.java
@@ -3,13 +3,13 @@ package com.github.makewheels.video2022.etc.exception;
 import com.github.makewheels.video2022.etc.response.ErrorCode;
 import com.github.makewheels.video2022.etc.response.Result;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
 
 /**
  * 全局异常处理
@@ -22,8 +22,17 @@ public class GlobalExceptionHandler {
 
     @ResponseBody
     @ExceptionHandler(Exception.class)
-    public Result<Void> exceptionHandler(HttpServletRequest request, Exception e) {
+    public Result<Void> exceptionHandler(Exception e) {
         e.printStackTrace();
+
+        //保存错误到数据库
+        ExceptionLog exceptionLog = new ExceptionLog();
+        String stackTrace = ExceptionUtils.getStackTrace(e);
+        exceptionLog.setStackTrace(stackTrace);
+        mongoTemplate.save(exceptionLog);
+
+        //返回前端错误信息
         return new Result<>(ErrorCode.FAIL.getCode(), e.getMessage());
     }
+
 }
---
+++ src/main/java/com/github/makewheels/video2022/etc/exception/VideoException.java
@@ -0,0 +1,4 @@
+package com.github.makewheels.video2022.etc.exception;
+
+public class VideoException extends RuntimeException{
+}
