c2756cb647edbd7b4b2a5df87b3f9969383ace84
makewheels <33256842+makewheels@users.noreply.github.com>
2023-02-27 23:13:15 +0800

    shortUrl跳转

---
+++ src/main/java/com/github/makewheels/video2022/watch/ShortIdController.java
@@ -0,0 +1,17 @@
+package com.github.makewheels.video2022.watch;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class ShortIdController {
+    @Value("${internal-base-url}")
+    private String baseUrl;
+
+    @GetMapping("{shortId}")
+    public String toWatchPage(@PathVariable("shortId") String shortId) {
+        return "redirect:" + baseUrl + "watch?v=" + shortId;
+    }
+}
