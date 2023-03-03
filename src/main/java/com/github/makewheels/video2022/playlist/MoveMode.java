ee5a2b0892bae0b861f5ba78908926351a79fe1a
makewheels <33256842+makewheels@users.noreply.github.com>
2023-03-04 03:13:07 +0800

    播放列表

--- src/main/java/com/github/makewheels/video2022/playlist/MoveMode.java
+++ src/main/java/com/github/makewheels/video2022/playlist/MoveMode.java
@@ -1,7 +1,9 @@
 package com.github.makewheels.video2022.playlist;
 
 public interface MoveMode {
-    String MOVE_TO_POSITION = "MOVE_TO_POSITION";
-    String MOVE_BEFORE_VIDEO = "MOVE_BEFORE_VIDEO";
-    String MOVE_AFTER_VIDEO = "MOVE_AFTER_VIDEO";
+    String TO_INDEX = "TO_INDEX";          // 移到指定索引位置
+    String BEFORE_VIDEO = "BEFORE_VIDEO";  // 移到指定视频之前
+    String AFTER_VIDEO = "AFTER_VIDEO";    // 移到指定视频之后
+    String TO_TOP = "TO_TOP";              // 移到播放列表最前面
+    String TO_BOTTOM = "TO_BOTTOM";        // 移到播放列表最后面
 }
