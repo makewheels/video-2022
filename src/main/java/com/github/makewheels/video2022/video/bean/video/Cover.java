82b690da9b89fa61ed68159f24b5490085a02fba
makewheels <33256842+makewheels@users.noreply.github.com>
2023-03-18 12:40:09 +0800

    去掉cover

---
+++ src/main/java/com/github/makewheels/video2022/video/bean/video/Cover.java
@@ -0,0 +1,13 @@
+package com.github.makewheels.video2022.video.bean.video;
+
+import lombok.Data;
+
+/**
+ * 封面
+ */
+@Data
+public class Cover {
+    private String id;
+    private String url;
+    private String key;
+}
