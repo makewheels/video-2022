74cefd1ff1f61fa7d3e536d54c1df50149fddedf
diypcfev <username@email.com>
2023-04-18 19:52:22 +0800

    封面签名url

---
+++ src/main/java/com/github/makewheels/video2022/cover/CoverService.java
@@ -0,0 +1,24 @@
+package com.github.makewheels.video2022.cover;
+
+import com.github.makewheels.video2022.file.FileService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.time.Duration;
+
+@Service
+public class CoverService {
+    @Resource
+    private CoverRepository coverRepository;
+    @Resource
+    private FileService fileService;
+
+    /**
+     * 根据coverId，生成带对象存储签名的url
+     */
+    public String getSignedCoverUrl(String coverId) {
+        Cover cover = coverRepository.getById(coverId);
+        if (cover == null) return null;
+        return fileService.generatePresignedUrl(cover.getKey(), Duration.ofHours(2));
+    }
+}
