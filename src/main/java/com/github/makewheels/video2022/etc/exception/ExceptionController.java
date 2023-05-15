e81562778c19f1133f69fb5f3af8984bf8a801d9
makewheels <33256842+makewheels@users.noreply.github.com>
2023-05-16 07:57:13 +0800

    改造钉钉NotificationService

--- src/main/java/com/github/makewheels/video2022/etc/exception/ExceptionController.java
+++ src/main/java/com/github/makewheels/video2022/etc/exception/ExceptionController.java
@@ -4,6 +4,7 @@ import com.github.makewheels.video2022.etc.response.Result;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -16,8 +17,8 @@ public class ExceptionController {
 
     @GetMapping
     @RequestMapping("getById")
-    public Result<ExceptionLog> getById(String id) {
-        ExceptionLog exceptionLog = mongoTemplate.findById(id, ExceptionLog.class);
+    public Result<ExceptionLog> getById(@RequestParam String exceptionLogId) {
+        ExceptionLog exceptionLog = mongoTemplate.findById(exceptionLogId, ExceptionLog.class);
         return Result.ok(exceptionLog);
     }
 }
