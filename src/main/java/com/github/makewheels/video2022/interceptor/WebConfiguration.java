a8e28a8c69aacfd9309e36371c6c92a9825d07fd
makewheels <33256842+makewheels@users.noreply.github.com>
2023-05-15 08:16:16 +0800

    getOssObjectMetadataByKey

--- src/main/java/com/github/makewheels/video2022/interceptor/WebConfiguration.java
+++ src/main/java/com/github/makewheels/video2022/interceptor/WebConfiguration.java
@@ -54,7 +54,7 @@ public class WebConfiguration implements WebMvcConfigurer {
 
                 .excludePathPatterns("/playlist/getPlayItemListDetail")
                 .excludePathPatterns("/sendmsg")
-                .excludePathPatterns("/file/getOssInfoByKey")
+                .excludePathPatterns("/file/getOssObjectMetadataByKey")
         ;
     }
 }
