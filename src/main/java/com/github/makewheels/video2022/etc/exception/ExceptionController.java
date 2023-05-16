da2f47929c0a3580850b3aceed3b873aac73fcd4
makewheels <33256842+makewheels@users.noreply.github.com>
2023-05-16 08:11:41 +0800

    新增获取异常log接口

--- src/main/java/com/github/makewheels/video2022/etc/exception/ExceptionController.java
+++ src/main/java/com/github/makewheels/video2022/etc/exception/ExceptionController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 
 @RestController
-@RequestMapping("exception")
+@RequestMapping("exceptionLog")
 public class ExceptionController {
     @Resource
     private MongoTemplate mongoTemplate;
