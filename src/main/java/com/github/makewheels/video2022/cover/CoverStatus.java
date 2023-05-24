7b3249563050f15f73c79f3d4af58dbb77fc3ea5
makewheels <33256842+makewheels@users.noreply.github.com>
2023-05-25 07:52:42 +0800

    CoverStatus改为interface

--- src/main/java/com/github/makewheels/video2022/cover/CoverStatus.java
+++ src/main/java/com/github/makewheels/video2022/cover/CoverStatus.java
@@ -1,6 +1,6 @@
 package com.github.makewheels.video2022.cover;
 
-public class CoverStatus {
-    public static final String CREATED = "CREATED";
-    public static final String READY = "READY";
+public interface CoverStatus {
+    String CREATED = "CREATED";
+    String READY = "READY";
 }
