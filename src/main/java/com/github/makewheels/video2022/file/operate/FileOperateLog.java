83e017949b7cac4b728b8a6214d298b4e66b1e53
makewheels <33256842+makewheels@users.noreply.github.com>
2023-05-24 08:21:15 +0800

    FileOperateLog.java

--- src/main/java/com/github/makewheels/video2022/file/operate/FileOperateLog.java
+++ src/main/java/com/github/makewheels/video2022/file/operate/FileOperateLog.java
@@ -4,6 +4,8 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.Date;
+
 /**
  * 文件操作日志
  */
@@ -12,4 +14,8 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class FileOperateLog {
     @Id
     private String id;
+
+    private String operationType;
+    private Date createTime;
+    private String operator;
 }
--- src/main/java/com/github/makewheels/video2022/video/constants/Visibility.java
+++ src/main/java/com/github/makewheels/video2022/video/constants/Visibility.java
@@ -4,9 +4,9 @@ package com.github.makewheels.video2022.video.constants;
  * 权限，模仿YouTube
  */
 public interface Visibility {
-    String PUBLIC = "PUBLIC";
-    String UNLISTED = "UNLISTED";
-    String PRIVATE = "PRIVATE";
+    String PUBLIC = "PUBLIC";       //全公开
+    String UNLISTED = "UNLISTED";   //拿到链接能看，但是在平台搜不到
+    String PRIVATE = "PRIVATE";     //需要登录自己的账号才能看
 
     String[] ALL = {PUBLIC, UNLISTED, PRIVATE};
 }
