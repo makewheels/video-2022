5e9fce2ca27e7967e27669bd1dad03b9bd1eb9fb
makewheels <33256842+makewheels@users.noreply.github.com>
2023-05-27 10:28:24 +0800

    阿里云云函数：获取OSS文件MD5

--- src/main/java/com/github/makewheels/video2022/file/md5/Md5CloudFunction.java
+++ src/main/java/com/github/makewheels/video2022/file/md5/Md5CloudFunction.java
@@ -3,7 +3,9 @@ package com.github.makewheels.video2022.file.md5;
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.io.IoUtil;
 import cn.hutool.crypto.digest.DigestUtil;
+import cn.hutool.http.HttpUtil;
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.aliyun.fc.runtime.Context;
 import com.aliyun.fc.runtime.HttpRequestHandler;
@@ -13,45 +15,90 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
- * https://fcnext.console.aliyun.com/cn-beijing/services/video-2022-prod/function-detail/get-oss-object-md5/LATEST?tab=trigger
- * https://fcnext.console.aliyun.com/cn-beijing/services/video-2022-dev/function-detail/get-oss-object-md5/LATEST?tab=trigger
+ * 阿里云云函数：获取OSS文件MD5
+ * <a href="https://fcnext.console.aliyun.com/cn-beijing/services/video-2022-prod/function-detail/get-oss-object-md5/LATEST?tab=trigger">prod获取md5云函数</a>
+ * <a href="https://fcnext.console.aliyun.com/cn-beijing/services/video-2022-dev/function-detail/get-oss-object-md5/LATEST?tab=trigger">dev获取md5云函数</a>
  */
 @Slf4j
 public class Md5CloudFunction implements HttpRequestHandler {
     // OSS挂载路径
     private final static String OSS_MOUNT_PATH = "/mnt/oss";
 
+    /**
+     * 获取请求body
+     */
+    private JSONObject getRequestBody(HttpServletRequest request) throws IOException {
+        JSONObject body = JSON.parseObject(IoUtil.readUtf8(request.getInputStream()));
+        log.info("请求body = " + body.toJSONString());
+        return body;
+    }
+
+    /**
+     * 获取keyList
+     */
+    private List<String> getKeyList(JSONObject body) {
+        JSONArray keyListJSONArray = body.getJSONArray("keyList");
+        return JSONArray.parseArray(keyListJSONArray.toJSONString(), String.class);
+    }
+
+    /**
+     * 云函数根据挂载的OSS文件计算MD5
+     */
+    private String getMd5(String key) {
+        File file = new File(OSS_MOUNT_PATH, key);
+        log.info("文件路径：" + file.getAbsolutePath());
+        long startTime = System.currentTimeMillis();
+        String md5 = DigestUtil.md5Hex(file);
+        log.info("计算MD5 = " + md5
+                + "，文件大小：" + FileUtil.readableFileSize(file)
+                + "耗时：" + (System.currentTimeMillis() - startTime) + "ms");
+        return md5;
+    }
+
+    /**
+     * 响应结果
+     */
+    private void responseResult(JSONObject body, HttpServletResponse response, Map<String, Object> resultMap)
+            throws IOException {
+        String result = JSON.toJSONString(resultMap);
+        log.info("响应结果：" + result);
+        // 同步返回
+        if (!body.getBoolean("isCallbackEnable")) {
+            response.setHeader("Content-Type", "application/json");
+            IoUtil.writeUtf8(response.getOutputStream(), true, result);
+            return;
+        }
+        // 异步回调
+        String callbackUrl = body.getString("callbackUrl");
+        HttpUtil.post(callbackUrl, result);
+    }
+
     @Override
     public void handleRequest(HttpServletRequest request, HttpServletResponse response, Context context)
             throws IOException {
-        // 拿到请求参数
-        JSONObject body = JSON.parseObject(IoUtil.readUtf8(request.getInputStream()));
-        log.info("body = " + body.toJSONString());
+        JSONObject body = getRequestBody(request);
+        List<String> keyList = getKeyList(body);
 
-        // 要计算md5的文件
-        String key = body.getString("key");
-        File file = new File(OSS_MOUNT_PATH, key);
-        log.info("文件路径：" + file.getAbsolutePath());
-        log.info("文件大小：" + FileUtil.readableFileSize(file));
-
-        // 计算md5
-        log.info("开始计算MD5：");
-        long startTime = System.currentTimeMillis();
-        String md5 = DigestUtil.md5Hex(file);
-        log.info("计算MD5结束，md5 = " + md5);
-        log.info("耗时：" + (System.currentTimeMillis() - startTime) + "ms");
+        List<Map<String, String>> objectList = new ArrayList<>(keyList.size());
+        for (String key : keyList) {
+            String md5 = getMd5(key);
+            Map<String, String> objectMap = new HashMap<>();
+            objectMap.put("key", key);
+            objectMap.put("md5", md5);
+            objectList.add(objectMap);
+        }
 
         //返回结果
         Map<String, Object> resultMap = new HashMap<>();
         resultMap.put("aliyunRequestId", context.getRequestId());
-        resultMap.put("key", key);
-        resultMap.put("md5", md5);
-        String result = JSON.toJSONString(resultMap);
-        response.setHeader("Content-Type", "application/json");
-        IoUtil.writeUtf8(response.getOutputStream(), true, result);
+        resultMap.put("objectList", objectList);
+        responseResult(body, response, resultMap);
     }
+
 }
