92904fe98b47fe18c5ca3ef8e785dde640891394
makewheels <33256842+makewheels@users.noreply.github.com>
2023-05-27 13:16:19 +0800

    FileMd5DTO

---
+++ src/main/java/com/github/makewheels/video2022/file/md5/FileMd5DTO.java
@@ -0,0 +1,10 @@
+package com.github.makewheels.video2022.file.md5;
+
+import lombok.Data;
+
+@Data
+public class FileMd5DTO {
+    private String fileId;
+    private String key;
+    private String md5;
+}
