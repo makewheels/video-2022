0dc21bdb877474d9996ab350b1b3baa5e7ced885
makewheels <33256842+makewheels@users.noreply.github.com>
2023-06-17 12:36:07 +0800

    把OSS源文件改为低频存储

--- src/main/java/com/github/makewheels/video2022/video/service/VideoReadyService.java
+++ src/main/java/com/github/makewheels/video2022/video/service/VideoReadyService.java
@@ -1,5 +1,6 @@
 package com.github.makewheels.video2022.video.service;
 
+import com.aliyun.oss.model.StorageClass;
 import com.github.makewheels.video2022.etc.ding.NotificationService;
 import com.github.makewheels.video2022.file.FileService;
 import com.github.makewheels.video2022.system.environment.EnvironmentService;
@@ -38,7 +39,7 @@ public class VideoReadyService {
 
         // 如果不是link，那就是第一次上传，把OSS源文件改为低频存储
         if (!video.getLink().getHasLink()) {
-
+            fileService.changeStorageClass(video.getRawFileId(), StorageClass.IA.toString());
         }
     }
 
