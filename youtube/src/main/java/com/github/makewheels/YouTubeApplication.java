f5ea35dd4b6df7dacc74224129e420d6b5425d63
makewheels <33256842+makewheels@users.noreply.github.com>
2023-07-09 06:38:36 +0800

    改运行dockerfile

--- youtube/src/main/java/com/github/makewheels/YouTubeApplication.java
+++ youtube/src/main/java/com/github/makewheels/YouTubeApplication.java
@@ -1,7 +1,11 @@
 package com.github.makewheels;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class YouTubeApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(YouTubeApplication.class, args);
     }
 }
