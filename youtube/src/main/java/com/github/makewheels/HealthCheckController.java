f4dac49b58244883a31e4cee069de9d87b8289e2
makewheels <33256842+makewheels@users.noreply.github.com>
2023-07-09 10:39:52 +0800

    云函数加入全局请求路径

--- youtube/src/main/java/com/github/makewheels/HealthCheckController.java
+++ youtube/src/main/java/com/github/makewheels/HealthCheckController.java
@@ -12,13 +12,15 @@ import java.util.Date;
 public class HealthCheckController {
     @GetMapping("2016-08-15/proxy/video-2022-hongkong.LATEST/video-2022-youtube/healthCheck")
     public String healthCheck1() {
-        log.info("healthCheck1-" + DateUtil.formatDateTime(new Date()));
-        return "ok " + System.currentTimeMillis();
+        String str = "healthCheck1-" + DateUtil.formatDateTime(new Date());
+        log.info(str);
+        return str;
     }
 
     @GetMapping("healthCheck")
     public String healthCheck2() {
-        log.info("healthCheck2-" + DateUtil.formatDateTime(new Date()));
-        return "ok " + System.currentTimeMillis();
+        String str = "healthCheck2-" + DateUtil.formatDateTime(new Date());
+        log.info(str);
+        return str;
     }
 }
