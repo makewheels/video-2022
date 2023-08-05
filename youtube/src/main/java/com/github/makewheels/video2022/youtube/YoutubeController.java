e9cb31224a3b88e9537496496d833ac982279fa5
makewheels <33256842+makewheels@users.noreply.github.com>
2023-08-05 11:23:03 +0800

    打印云函数日志

--- youtube/src/main/java/com/github/makewheels/video2022/youtube/YoutubeController.java
+++ youtube/src/main/java/com/github/makewheels/video2022/youtube/YoutubeController.java
@@ -1,12 +1,15 @@
 package com.github.makewheels.video2022.youtube;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
 @RestController
 @RequestMapping("youtube")
+@Slf4j
 public class YoutubeController {
     @Resource
     private YoutubeService youtubeService;
@@ -21,17 +24,26 @@ public class YoutubeController {
 
     @GetMapping("getVideoInfo")
     public JSONObject getVideoInfo(@RequestParam String youtubeVideoId) {
-        return youtubeService.getVideoInfo(youtubeVideoId);
+        log.info("getVideoInfo: 请求youtubeVideoId = " + youtubeVideoId);
+        JSONObject videoInfo = youtubeService.getVideoInfo(youtubeVideoId);
+        log.info("getVideoInfo: 返回videoInfo = " + JSON.toJSONString(videoInfo));
+        return videoInfo;
     }
 
     @PostMapping("transferVideo")
     public JSONObject transferVideo(@RequestBody JSONObject body) {
-        return youtubeService.transferVideo(body);
+        log.info("transferVideo: 请求body = " + JSON.toJSONString(body));
+        JSONObject jsonObject = youtubeService.transferVideo(body);
+        log.info("transferVideo: 返回jsonObject = " + JSON.toJSONString(jsonObject));
+        return jsonObject;
     }
 
     @PostMapping("transferFile")
     public JSONObject transferFile(@RequestBody JSONObject body) {
-        return youtubeService.transferFile(body);
+        log.info("transferFile: 请求body = " + JSON.toJSONString(body));
+        JSONObject jsonObject = youtubeService.transferFile(body);
+        log.info("transferFile: 返回jsonObject = " + JSON.toJSONString(jsonObject));
+        return jsonObject;
     }
 
 }
