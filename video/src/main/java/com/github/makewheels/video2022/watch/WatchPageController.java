547e81b1e865ead7196c153be6047dcaec6a3fe8
makewheels <33256842+makewheels@users.noreply.github.com>
2023-08-05 09:17:52 +0800

    修改watch url

--- video/src/main/java/com/github/makewheels/video2022/watch/WatchPageController.java
+++ video/src/main/java/com/github/makewheels/video2022/watch/WatchPageController.java
@@ -1,12 +1,23 @@
 package com.github.makewheels.video2022.watch;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WatchPageController {
     @RequestMapping("/watch")
     public String watch() {
+        return "watch";
+    }
+
+    @RequestMapping("/w")
+    public String w() {
+        return "watch";
+    }
+
+    @RequestMapping("/{watchId}")
+    public String pathVariable(@PathVariable String watchId) {
         return "watch";
     }
 }
