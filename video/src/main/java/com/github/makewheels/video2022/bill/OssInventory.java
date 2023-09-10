feb94483bbef165544ec6124f11383e78e3daf4e
makewheels <33256842+makewheels@users.noreply.github.com>
2023-09-10 21:38:44 +0800

    准备接入对象存储快照，做计费结算功能

---
+++ video/src/main/java/com/github/makewheels/video2022/bill/OssInventory.java
@@ -0,0 +1,21 @@
+package com.github.makewheels.video2022.bill;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * oss清单
+ * <a href="https://help.aliyun.com/zh/oss/user-guide/when-inventories-take-effect">清单</a>
+ */
+@Data
+public class OssInventory {
+    private String bucketName;
+    private String objectName;
+    private Long size;
+    private String storageClass;
+    private Date lastModifiedDate;
+    private String eTag;
+    private Boolean isMultipartUploaded;
+    private Boolean encryptionStatus;
+}
