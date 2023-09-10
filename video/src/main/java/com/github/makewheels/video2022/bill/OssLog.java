c24d1557abb3b67fd69a3e81dbc2092eac287746
makewheels <33256842+makewheels@users.noreply.github.com>
2023-09-10 21:38:44 +0800

    准备接入对象存储快照，做计费结算功能

---
+++ video/src/main/java/com/github/makewheels/video2022/bill/OssLog.java
@@ -0,0 +1,41 @@
+package com.github.makewheels.video2022.bill;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * oss日志
+ * <a href="https://help.aliyun.com/zh/oss/user-guide/logging">日志转存</a>
+ */
+@Data
+public class OssLog {
+    private String remoteIp;
+    private String reserved1;
+    private String reserved2;
+    private Date time;
+    private String requestUrl;
+    private Integer httpStatus;
+    private Long sentBytes;
+    private Long requestTime;
+    private String referer;
+    private String userAgent;
+    private String hostName;
+    private String requestId;
+    private Boolean loggingFlag;
+    private String requesterAliyunId;
+    private String operation;
+    private String bucketName;
+    private String objectName;
+    private Long objectSize;
+    private Long serverCostTime;
+    private String errorCode;
+    private Integer requestLength;
+    private String userId;
+    private Long deltaDataSize;
+    private String syncRequest;
+    private String storageClass;
+    private String targetStorageClass;
+    private String transmissionAccelerationAccessPoint;
+    private String accessKeyID;
+}
