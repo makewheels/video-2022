75566ebedfa81a63be9c36882cf3834ed4b6b7c5
makewheels <33256842+makewheels@users.noreply.github.com>
2023-09-13 02:13:17 +0800

    包结构调整

--- video/src/main/java/com/github/makewheels/video2022/file/oss/inventory/OssInventoryService.java
+++ video/src/main/java/com/github/makewheels/video2022/file/oss/inventory/OssInventoryService.java
@@ -1,24 +1,27 @@
 package com.github.makewheels.video2022.file.oss.inventory;
 
 import cn.hutool.core.date.DateUtil;
-import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.text.csv.CsvData;
 import cn.hutool.core.text.csv.CsvRow;
 import cn.hutool.core.text.csv.CsvUtil;
 import cn.hutool.core.util.URLUtil;
-import cn.hutool.http.HttpUtil;
+import cn.hutool.extra.compress.CompressUtil;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.aliyun.oss.model.OSSObjectSummary;
+import com.github.makewheels.video2022.etc.springboot.exception.VideoException;
 import com.github.makewheels.video2022.file.oss.OssDataService;
-import org.apache.commons.collections4.CollectionUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.util.Assert;
+import sun.util.calendar.ZoneInfo;
 
 import javax.annotation.Resource;
 import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -28,6 +31,7 @@ import java.util.stream.Collectors;
  * oss清单解析
  */
 @Service
+@Slf4j
 public class OssInventoryService {
 
     @Value("${aliyun.oss.data.inventory-prefix}")
@@ -40,60 +44,63 @@ public class OssInventoryService {
      * 获取manifest.json的key
      */
     private String getManifestKey(Date date) {
-        // 把传入时间转为UTC日期
-        String utcDate = DateUtil.format(date, "yyyy-MM-dd'T'HH-mm'Z'");
-        // 获取UTC日期
-        String utcDatePrefix = utcDate.substring(0, 10);
-        List<OSSObjectSummary> ossObjectSummaries = ossDataService.listAllObjects(
-                inventoryPrefix + "/" + utcDatePrefix);
-        if (CollectionUtils.isEmpty(ossObjectSummaries)) {
-            return null;
-        }
-        OSSObjectSummary ossObjectSummary = ossObjectSummaries.stream()
+        log.info("获取manifest.json的key, 传入的时间 = " + DateUtil.formatDateTime(date));
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        simpleDateFormat.setTimeZone(ZoneInfo.getTimeZone("UTC"));
+        String utcDate = simpleDateFormat.format(date);
+        List<OSSObjectSummary> ossObjectList
+                = ossDataService.listAllObjects(inventoryPrefix + "/" + utcDate);
+        OSSObjectSummary ossObjectSummary = ossObjectList.stream()
                 .filter(e -> FilenameUtils.getName(e.getKey()).equals("manifest.json"))
-                .findFirst().orElse(null);
-        Assert.notNull(ossObjectSummary, "找不到manifest.json");
+                .findFirst().orElseThrow(() -> new VideoException("找不到manifest.json"));
+        log.info("找到manifest.json, ossObject = " + JSON.toJSONString(ossObjectSummary));
         return ossObjectSummary.getKey();
     }
 
     /**
      * 获取清单文件的key
      * {
-     *     "creationTimestamp": "1694305241",
-     *     "destinationBucket": "oss-data-bucket",
-     *     "fileFormat": "CSV",
-     *     "fileSchema": "Bucket, Key, Size, StorageClass, LastModifiedDate, ETag,
-     *                    IsMultipartUploaded, EncryptionStatus",
-     *     "files": [{
-     *             "MD5checksum": "5FC695B803A384A2FAA01D747C405FD1",
-     *             "key": "video-2022-dev/inventory/video-2022-dev/inventory-rule
-     *                     /data/2a755fbb-b920-4347-b99b-add10258972e.csv.gz",
-     *             "size": 12586
-     *      }],
-     *     "sourceBucket": "video-2022-dev",
-     *     "version": "2019-09-01"
+     * "creationTimestamp": "1694305241",
+     * "destinationBucket": "oss-data-bucket",
+     * "fileFormat": "CSV",
+     * "fileSchema": "Bucket, Key, Size, StorageClass, LastModifiedDate, ETag,
+     * IsMultipartUploaded, EncryptionStatus",
+     * "files": [{
+     * "MD5checksum": "5FC695B803A384A2FAA01D747C405FD1",
+     * "key": "video-2022-dev/inventory/video-2022-dev/inventory-rule
+     * /data/2a755fbb-b920-4347-b99b-add10258972e.csv.gz",
+     * "size": 12586
+     * }],
+     * "sourceBucket": "video-2022-dev",
+     * "version": "2019-09-01"
      * }
      */
     public List<String> getInventoryKeys(String manifestKey) {
-        // TODO 获取manifest.json文件
-        JSONObject manifest = new JSONObject();
+        String json = ossDataService.getObjectContent(manifestKey);
+        JSONObject manifest = JSON.parseObject(json);
         return manifest.getJSONArray("files").stream()
                 .map(e -> ((JSONObject) e).getString("key"))
                 .collect(Collectors.toList());
     }
 
     /**
-     * 通过inventory获取
+     * 获取csv文件
      */
-    public List<File> getCsvFiles(String gzKeys){
-        // 下载gz文件
-        File tempFile = FileUtil.createTempFile();
-        HttpUtil.downloadFile("", tempFile);
+    public List<File> getCsvFiles(List<String> gzKeys) {
+        List<File> csvFiles = new ArrayList<>(gzKeys.size());
+        for (String gzKey : gzKeys) {
+            // 下载gz文件
+            File gzFile = new File(FileUtils.getTempDirectory(), FilenameUtils.getName(gzKey));
+            // 解压gz文件
+            CompressUtil.createExtractor(StandardCharsets.UTF_8, gzFile)
+                    .extract(gzFile.getParentFile());
+            // TODO 怎么找到csv文件
+            File csvFile = new File(gzFile.getParentFile(), "data.csv");
+            gzFile.delete();
+            csvFiles.add(csvFile);
+        }
 
-
-        // 解压gz文件
-
-        return null;
+        return csvFiles;
     }
 
     /**
@@ -118,12 +125,4 @@ public class OssInventoryService {
         return inventoryList;
     }
 
-    public static void main(String[] args) {
-        File file = new File("C:\\Users\\thedoflin\\Downloads\\" +
-                "7b6f5016-7cd5-4d73-99d4-8257902cfc4a.csv");
-        List<OssInventory> inventoryList = new OssInventoryService().parseFileToInventory(file);
-        for (OssInventory ossInventory : inventoryList) {
-            System.out.println(JSON.toJSONString(ossInventory, true));
-        }
-    }
 }
