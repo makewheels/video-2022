3ef43902630a2a9fc26f32cd63888733b24d3b03
makewheels <33256842+makewheels@users.noreply.github.com>
2023-12-18 00:32:39 +0800

    新增播放进度功能

--- video/src/main/java/com/github/makewheels/video2022/watch/progress/ProgressRepository.java
+++ video/src/main/java/com/github/makewheels/video2022/watch/progress/ProgressRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.Assert;
 
 import javax.annotation.Resource;
 
@@ -13,11 +14,12 @@ public class ProgressRepository {
     private MongoTemplate mongoTemplate;
 
     public Progress getProgress(String videoId, String viewerId, String clientId) {
+        Assert.isTrue(viewerId != null || clientId != null,
+                "viewerId和clientId不能同时为空");
         Criteria criteria = Criteria.where("videoId").is(videoId);
         if (viewerId != null) {
             criteria.and("viewerId").is(viewerId);
-        }
-        if (clientId != null) {
+        } else {
             criteria.and("clientId").is(clientId);
         }
         return mongoTemplate.findOne(new Query(criteria), Progress.class);
