c882a788571360f90b86430279b85dda0e50d3cd
makewheels <33256842+makewheels@users.noreply.github.com>
2023-12-22 16:29:28 +0800

    解析OSS访问日志

--- video/src/main/java/com/github/makewheels/video2022/oss/osslog/OssLogRepository.java
+++ video/src/main/java/com/github/makewheels/video2022/oss/osslog/OssLogRepository.java
@@ -1,5 +1,6 @@
 package com.github.makewheels.video2022.oss.osslog;
 
+import com.github.makewheels.video2022.oss.osslog.bean.OssAccessLog;
 import com.github.makewheels.video2022.oss.osslog.bean.OssAccessLogFile;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
@@ -20,6 +21,16 @@ public class OssLogRepository {
         return mongoTemplate.exists(
                 Query.query(Criteria.where("logFileKey").is(logFileKey)),
                 OssAccessLogFile.class);
+    }
+
+
+    /**
+     * 一条记录的md5是否存在
+     */
+    public boolean isOssLogLineMd5Exists(String md5) {
+        return mongoTemplate.exists(
+                Query.query(Criteria.where("md5").is(md5)),
+                OssAccessLog.class);
     }
 
 }
