cda23d3c896e6d8ede95b7d4fa5dfc575952248b
diypcfev <username@email.com>
2023-12-22 17:44:00 +0800

    解析OSS访问日志

--- video/src/main/java/com/github/makewheels/video2022/oss/osslog/OssLogController.java
+++ video/src/main/java/com/github/makewheels/video2022/oss/osslog/OssLogController.java
@@ -1,11 +1,13 @@
 package com.github.makewheels.video2022.oss.osslog;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
+import java.util.Map;
 import javax.annotation.Resource;
 
 @RestController
@@ -18,10 +20,12 @@ public class OssLogController {
      * 根据时间生成OSS访问日志
      */
     @GetMapping("generateOssAccessLog")
-    public void generateOssAccessLog(@RequestParam("startDate") LocalDate startDate,
-                                     @RequestParam("endDate") LocalDate endDate) {
+    public void generateOssAccessLog(@RequestParam("startDate") String startDateString,
+                                     @RequestParam("endDate") String endDateString) {
+        LocalDate startDate = LocalDate.parse(startDateString);
+        LocalDate endDate = LocalDate.parse(endDateString);
         LocalDate currentDate = startDate;
-        while (currentDate.isBefore(endDate)) {
+        while (!currentDate.isAfter(endDate)) {
             ossLogService.generateOssAccessLog(currentDate);
             currentDate = currentDate.plusDays(1);
         }
