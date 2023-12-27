e5abe00ed1688c68f053f7880ab844c15b749b2a
makewheels <33256842+makewheels@users.noreply.github.com>
2023-12-27 17:44:19 +0800

    加了几种视频状态

--- video/src/main/java/com/github/makewheels/video2022/video/constants/VideoStatus.java
+++ video/src/main/java/com/github/makewheels/video2022/video/constants/VideoStatus.java
@@ -2,14 +2,14 @@ package com.github.makewheels.video2022.video.constants;
 
 import org.apache.commons.lang3.StringUtils;
 
-public interface VideoStatus {
-    String CREATED = "CREATED";
-    String UPLOADING = "UPLOADING";
-    String PREPARE_TRANSCODING = "PREPARE_TRANSCODING";
-    String TRANSCODING = "TRANSCODING";
-    String TRANSCODING_PARTLY_COMPLETE = "TRANSCODING_PARTLY_COMPLETE";
-    String PROCESSING_AFTER_TRANSCODE_COMPLETE = "PROCESSING_AFTER_TRANSCODE_COMPLETE";
-    String READY = "READY";
+public class VideoStatus {
+    public static final String CREATED = "CREATED";
+    public static final String UPLOADING = "UPLOADING";
+    public static final String PREPARE_TRANSCODING = "PREPARE_TRANSCODING";
+    public static final String TRANSCODING = "TRANSCODING";
+    public static final String TRANSCODING_PARTLY_COMPLETE = "TRANSCODING_PARTLY_COMPLETE";
+    public static final String PROCESSING_AFTER_TRANSCODE_COMPLETE = "PROCESSING_AFTER_TRANSCODE_COMPLETE";
+    public static final String READY = "READY";
 
     /**
      * 是就绪状态
