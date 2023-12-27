ff7842b357c2450ef1f836ff535e5fd102b7729e
makewheels <33256842+makewheels@users.noreply.github.com>
2023-12-27 18:01:04 +0800

    文件操作类型

---
+++ video/src/main/java/com/github/makewheels/video2022/file/operate/FileChange.java
@@ -0,0 +1,35 @@
+package com.github.makewheels.video2022.file.operate;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+/**
+ * 对象存储文件变更记录
+ */
+@Data
+@Document
+public class FileChange {
+    @Id
+    private String id;
+
+    @Indexed
+    private String fileId;
+    @Indexed
+    private String key;
+
+    private String method;
+    private String operator;
+
+    private String beforeStorageClass;   // 改动前的存储类型
+    private String afterStorageClass;    // 改动后的存储类型
+
+    private String beforeAcl;   // 改动前的ACL
+    private String afterAcl;    // 改动后的ACL
+
+
+    private Date createTime;
+}
--- video/src/main/java/com/github/makewheels/video2022/file/operate/FileChangeService.java
+++ video/src/main/java/com/github/makewheels/video2022/file/operate/FileChangeService.java
@@ -3,5 +3,5 @@ package com.github.makewheels.video2022.file.operate;
 import org.springframework.stereotype.Service;
 
 @Service
-public class FileOperateLogService {
+public class FileChangeService {
 }
