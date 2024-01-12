c5c290dd7e5fb4a9195edf23f6ef06e8c5c1adaf
makewheels <33256842+makewheels@users.noreply.github.com>
2024-01-12 16:42:03 +0800

    RedisService取消过时

--- video/src/main/java/com/github/makewheels/video2022/etc/redis/RedisService.java
+++ video/src/main/java/com/github/makewheels/video2022/etc/redis/RedisService.java
@@ -2,21 +2,19 @@ package com.github.makewheels.video2022.etc.redis;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-import com.aliyuncs.endpoint.LocalConfigGlobalEndpointResolver;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.Resource;
 import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
+import javax.annotation.Resource;
 
 /**
  * Redis工具类
  */
 @Service
-@Deprecated
 @Slf4j
 public class RedisService {
     @Resource
